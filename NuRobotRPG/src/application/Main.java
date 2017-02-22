bc1fb764060f7861ce73dec7c64099793a20f47a
ZSnowbank <zsnowbank@student.neumont.edu>
2017-02-22 14:36:52 -0700

    Added the Map Class and Room Class used within the Map Class

--- NuRobotRPG/src/application/Main.java
+++ NuRobotRPG/src/application/Main.java
@@ -6,22 +6,23 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
 
-public class Main extends Application {	
+public class Main /*extends Application*/ {	
 	
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+//	@Override
+//	public void start(Stage primaryStage) {
+//		try {
+//			BorderPane root = new BorderPane();
+//			Scene scene = new Scene(root,400,400);
+//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+//			primaryStage.setScene(scene);
+//			primaryStage.show();
+//		} catch(Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
 	
 	public static void main(String[] args) {
-		launch(args);
+//		launch(args);
+	Test.run();
 	}
 }
