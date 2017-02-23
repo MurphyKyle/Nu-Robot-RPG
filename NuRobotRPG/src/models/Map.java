1074e4cef050767c43facad85440e28aa8a8f31d
ZSnowbank <zsnowbank@student.neumont.edu>
2017-02-23 14:12:04 -0700

    Added Engine.class. Added getters and setters to Map.class.

--- NuRobotRPG/src/models/Map.java
+++ NuRobotRPG/src/models/Map.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Map {
 	private Room[][] rooms;
 
-	public Map() {
+	public Map(int difficulty) {
 		// choose random size for map
 		Random randy = new Random();
 		int size = randy.nextInt(11) + 10;
@@ -19,14 +19,27 @@ public class Map {
 		// randomize the isOccupieds of rooms
 		// randomize depots of rooms
 		int depots = (int) Math.pow(size, 2) / 15 + 1;
-		int enemies = (int) Math.pow(size, 2) / 3;
+		int enemies;
+		switch (difficulty) {
+		case 1:
+			enemies = (int) Math.pow(size, 2) / 3;
+			break;
+		case 2:
+			enemies = (int) Math.pow(size, 2) * 2 / 5;
+			break;
+		case 3:
+			enemies = (int) Math.pow(size, 2) / 2;
+			break;
+		default:
+			enemies = 0;
+		}
 		for (int i = 0; i < depots; i++) {
 			boolean empty = true;
 			Room chosenRoom;
 			do {
 				empty = true;
 				chosenRoom = rooms[randy.nextInt(size)][randy.nextInt(size)];
-				if(chosenRoom.isDepot() || chosenRoom.isOccupied()) {
+				if (chosenRoom.isDepot() || chosenRoom.isOccupied()) {
 					empty = false;
 				}
 			} while (!empty);
@@ -36,9 +49,9 @@ public class Map {
 			boolean empty = true;
 			Room chosenRoom;
 			do {
-				empty= true;
+				empty = true;
 				chosenRoom = rooms[randy.nextInt(size)][randy.nextInt(size)];
-				if(chosenRoom.isDepot() || chosenRoom.isOccupied()) {
+				if (chosenRoom.isDepot() || chosenRoom.isOccupied()) {
 					empty = false;
 				}
 			} while (!empty);
@@ -46,7 +59,7 @@ public class Map {
 		}
 	}
 
-	public Map(int size) {
+	public Map(int difficulty, int size) {
 		Random randy = new Random();
 		// use int size to make map
 		// construct rooms
@@ -57,40 +70,48 @@ public class Map {
 			}
 		}
 		// randomize the isOccupieds of rooms
-				// randomize depots of rooms
-				int depots = (int) Math.pow(size, 2) / 15;
-				int enemies = (int) Math.pow(size, 2) / 3;
-				for (int i = 0; i < depots; i++) {
-					boolean empty = true;
-					Room chosenRoom;
-					do {
-						empty = true;
-						chosenRoom = rooms[randy.nextInt(size)][randy.nextInt(size)];
-						if(chosenRoom.isDepot() || chosenRoom.isOccupied()) {
-							empty = false;
-						}
-					} while (!empty);
-					chosenRoom.setDepot(true);
+		// randomize depots of rooms
+		int depots = (int) Math.pow(size, 2) / 15;
+		int enemies = (int) Math.pow(size, 2) / 3;
+		for (int i = 0; i < depots; i++) {
+			boolean empty = true;
+			Room chosenRoom;
+			do {
+				empty = true;
+				chosenRoom = rooms[randy.nextInt(size)][randy.nextInt(size)];
+				if (chosenRoom.isDepot() || chosenRoom.isOccupied()) {
+					empty = false;
 				}
-				for (int i = 0; i < enemies; i++) {
-					boolean empty = true;
-					Room chosenRoom;
-					do {
-						empty= true;
-						chosenRoom = rooms[randy.nextInt(size)][randy.nextInt(size)];
-						if(chosenRoom.isDepot() || chosenRoom.isOccupied()) {
-							empty = false;
-						}
-					} while (!empty);
-					chosenRoom.setOccupied(true);
+			} while (!empty);
+			chosenRoom.setDepot(true);
+		}
+		for (int i = 0; i < enemies; i++) {
+			boolean empty = true;
+			Room chosenRoom;
+			do {
+				empty = true;
+				chosenRoom = rooms[randy.nextInt(size)][randy.nextInt(size)];
+				if (chosenRoom.isDepot() || chosenRoom.isOccupied()) {
+					empty = false;
 				}
+			} while (!empty);
+			chosenRoom.setOccupied(true);
+		}
 	}
-	
+
+	public Room[][] getRooms() {
+		return rooms;
+	}
+
+	public void setRooms(Room[][] rooms) {
+		this.rooms = rooms;
+	}
+
 	@Override
 	public String toString() {
 		StringBuilder bob = new StringBuilder();
-		for(Room[] rArray : rooms) {
-			for(Room r : rArray) {
+		for (Room[] rArray : rooms) {
+			for (Room r : rArray) {
 				bob.append("[");
 				bob.append(r.isOccupied() ? "!" : r.isDepot() ? "D" : " ");
 				bob.append("]");
