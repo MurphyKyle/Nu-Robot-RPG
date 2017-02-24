432e6e4e5bfb43123d49f8ad1c9a18cf9c73f1f5
ZSnowbank <zsnowbank@student.neumont.edu>
2017-02-24 13:46:01 -0700

    Added combat method to Engine.class. Added enterRoom method to Room.class.

--- NuRobotRPG/src/models/Room.java
+++ NuRobotRPG/src/models/Room.java
@@ -1,18 +1,34 @@
 package models;
 
+import application.Engine;
+
 public class Room {
 	private boolean isOccupied;
 	private boolean isDepot;
-	
+
+	public void enterRoom(Robot player, int difficulty) {
+		if (this.isOccupied()) {
+			// Construct Enemy
+			Robot enemy = new Robot(difficulty);
+			// fight
+			Engine.fight(player, enemy);
+		} else if (this.isDepot()) {
+			// Depot Menu
+		}
+	}
+
 	public boolean isOccupied() {
 		return isOccupied;
 	}
+
 	public void setOccupied(boolean isOccupied) {
 		this.isOccupied = isOccupied;
 	}
+
 	public boolean isDepot() {
 		return isDepot;
 	}
+
 	public void setDepot(boolean isDepot) {
 		this.isDepot = isDepot;
 	}
