c57c52d530c81009c2f384c9b2b4af20d45710a8
ZSnowbank <zsnowbank@student.neumont.edu>
2017-03-01 13:37:55 -0700

    Added a constructor to Arm.java that takes in a string from the save file

--- NuRobotRPG/src/models/Arm.java
+++ NuRobotRPG/src/models/Arm.java
@@ -5,43 +5,45 @@ import java.util.Random;
 import enums.Rarity;
 import enums.Type;
 
-public class Arm extends Part{
+public class Arm extends Part {
 
 	private Type attackType;
 	private String function;
-	
-	public Arm(int difficulty){
+
+	public Arm(int difficulty) {
 		Random rand = new Random();
-		int rare = rand.nextInt(100)+1;
-		if(difficulty == 1){
-			if(rare <= 75){
+		int rare = rand.nextInt(100) + 1;
+		if (difficulty == 1) {
+			if (rare <= 75) {
 				setRarity(Rarity.COMMON);
-			}else{
+			} else {
 				setRarity(Rarity.UNCOMMON);
 			}
-		}else if(difficulty == 2){
-			if(rare <= 25){
+		} else if (difficulty == 2) {
+			if (rare <= 25) {
 				setRarity(Rarity.COMMON);
-			}else if(rare <= 75){
+			} else if (rare <= 75) {
 				setRarity(Rarity.UNCOMMON);
-			}else if(rare <= 99){
+			} else if (rare <= 99) {
 				setRarity(Rarity.RARE);
-			}else{
+			} else {
 				setRarity(Rarity.EXPERIMENTAL);
 			}
-		}else{
-			if(rare <= 50){
+		} else {
+			if (rare <= 50) {
 				setRarity(Rarity.UNCOMMON);
-			}else if(rare <= 90){
+			} else if (rare <= 90) {
 				setRarity(Rarity.RARE);
-			}else{
+			} else {
 				setRarity(Rarity.EXPERIMENTAL);
 			}
 		}
-		String[] prefixes = {"Hydraulic ","Plated ","Composite ","Pheonix MK 1 ","Durasteel ","Guerilla ","Variable-Cored "};
-		String[] suffixes = {"Tri-finger","Brawlfist","Palm Cannon","Mechacarpus","Digihand","Grip Chassis","Claw"};
+		String[] prefixes = { "Hydraulic ", "Plated ", "Composite ", "Pheonix MK 1 ", "Durasteel ", "Guerilla ",
+				"Variable-Cored " };
+		String[] suffixes = { "Tri-finger", "Brawlfist", "Palm Cannon", "Mechacarpus", "Digihand", "Grip Chassis",
+				"Claw" };
 		StringBuilder bob = new StringBuilder();
-		if(this.rarity == Rarity.EXPERIMENTAL) {
+		if (this.rarity == Rarity.EXPERIMENTAL) {
 			bob.append("Pheonix MK 2 ");
 		} else {
 			bob.append(prefixes[rand.nextInt(prefixes.length)]);
@@ -49,9 +51,9 @@ public class Arm extends Part{
 		bob.append(suffixes[rand.nextInt(suffixes.length)]);
 		this.name = bob.toString();
 		float multiplier = 1.0f;
-		for(int i = 0; i < Rarity.values().length; i++){
-			if(getRarity() == Rarity.values()[i]){
-				multiplier += (float)(i*.1);
+		for (int i = 0; i < Rarity.values().length; i++) {
+			if (getRarity() == Rarity.values()[i]) {
+				multiplier += (float) (i * .1);
 			}
 		}
 		setMultiplier(multiplier);
@@ -59,42 +61,61 @@ public class Arm extends Part{
 		rare = rand.nextInt(attacks.length);
 		setAttackType(attacks[rare]);
 		setFunction("Attack with " + getAttackType().toString());
-		rare = rand.nextInt(4)+4;
+		rare = rand.nextInt(4) + 4;
 		setWeight(rare);
 		setPartType("Arm");
 	}
-	
-	public Arm(String name, int weight, Rarity rare, Type attackType, String function){
+
+	public Arm(String name, int weight, Rarity rare, Type attackType, String function) {
 		setAttackType(attackType);
 		setName(name);
 		setFunction(function);
 		setRarity(rare);
 		setWeight(weight);
 		float multiplier = 1.0f;
-		for(int i = 0; i < Rarity.values().length; i++){
-			if(getRarity() == Rarity.values()[i]){
-				multiplier += (float)(i*.1);
+		for (int i = 0; i < Rarity.values().length; i++) {
+			if (getRarity() == Rarity.values()[i]) {
+				multiplier += (float) (i * .1);
 			}
 		}
 		setMultiplier(multiplier);
 		setPartType("Arm");
 	}
-	
+
+	public Arm(String input) {
+		this(input.split(" :: ")[0], Integer.parseInt(input.split(" :: ")[1]),
+				Rarity.COMMON.toString().equals(input.split(" :: ")[2]) ? Rarity.COMMON
+						: Rarity.UNCOMMON.toString().equals(input.split(" :: ")[2]) ? Rarity.UNCOMMON
+								: Rarity.RARE.toString().equals(input.split(" :: ")[2]) ? Rarity.RARE
+										: Rarity.EXPERIMENTAL,
+				// I'm so sorry but I had to use a triple nested ternary. There's no
+				// other way
+				Type.BALLISTIC.toString().equals(input.split(" :: ")[3]) ? Type.BALLISTIC
+						: Type.BEAM.toString().equals(input.split(" :: ")[3]) ? Type.BEAM
+								: Type.ELECTRICITY.toString().equals(input.split(" :: ")[3]) ? Type.ELECTRICITY
+										: Type.EXPLOSIVE.toString().equals(input.split(" :: ")[3]) ? Type.EXPLOSIVE
+												: Type.FIRE,
+				input.split(" :: ")[4]);
+	}
+
 	public Type getAttackType() {
 		return attackType;
 	}
+
 	public void setAttackType(Type attackType) {
 		this.attackType = attackType;
 	}
+
 	public String getFunction() {
 		return function;
 	}
+
 	public void setFunction(String function) {
 		this.function = function;
 	}
 
 	@Override
-	public String toString(){
+	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		sb.append("Arm\n");
 		sb.append(getName());
